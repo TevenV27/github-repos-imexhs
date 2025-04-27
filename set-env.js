03271f5a1f294984a7df4c7a9af4601570c1910f
Steven Victoria <102372427+TevenV27@users.noreply.github.com>
2025-04-27 16:05:33 -0500

    fead: Se corrije inconcistencias encontradas en cuanto a diseño

--- set-env.js
+++ set-env.js
@@ -13,12 +13,12 @@ const file = `
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "${process.env.NG_APP_FIREBASE_API_KEY}",
-    authDomain: "${process.env.NG_APP_FIREBASE_AUTH_DOMAIN}",
-    projectId: "${process.env.NG_APP_FIREBASE_PROJECT_ID}",
-    storageBucket: "${process.env.NG_APP_FIREBASE_STORAGE_BUCKET}",
-    messagingSenderId: "${process.env.NG_APP_FIREBASE_MESSAGING_SENDER_ID}",
-    appId: "${process.env.NG_APP_FIREBASE_APP_ID}"
+    apiKey: "${process.env.FIREBASE_API_KEY}",
+    authDomain: "${process.env.FIREBASE_AUTH_DOMAIN}",
+    projectId: "${process.env.FIREBASE_PROJECT_ID}",
+    storageBucket: "${process.env.FIREBASE_STORAGE_BUCKET}",
+    messagingSenderId: "${process.env.NFIREBASE_MESSAGING_SENDER_ID}",
+    appId: "${process.env.FIREBASE_APP_ID}"
   }
 };
 `;
